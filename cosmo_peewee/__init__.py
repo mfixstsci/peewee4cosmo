2db5c043e6f4ea522a3aa9c5d71e58a86d380baf
Mees Fix <mfix@stsci.edu>
2018-08-02 10:33:35 -0400

    Just ignoring webapp by commenting out import in __init__

--- cosmo_peewee/__init__.py
+++ cosmo_peewee/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 
 from .filesystem import *
 from .database import *
-from .webapp import *
+# from .webapp import *
 from .dark import *
 from .hv_level import *
